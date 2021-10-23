6e4c346900e43f527498027d8dc53b5c39b30954
maspriyambodo <maspriyambodo@gmail.com>
2021-10-23 21:28:50 +0700

    Changes to be committed: 	modified:   application/modules/Streaming/views/v_dds2.php

--- application/modules/Streaming/views/v_dds2.php
+++ application/modules/Streaming/views/v_dds2.php
@@ -10,6 +10,6 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/0ey2qqSu_Jg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="<?php echo $materi[0]->link_video; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </body>
 </html>
