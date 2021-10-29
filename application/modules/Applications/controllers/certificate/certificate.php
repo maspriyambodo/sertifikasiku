b230913854528e4fa0fbcb5e6fa230b4256ff084
maspriyambodo <maspriyambodo@gmail.com>
2021-10-29 15:37:18 +0700

    oasdpomadciad

---
+++ application/modules/Applications/controllers/certificate/certificate.php
@@ -0,0 +1,98 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class certificate extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('M_certificate', 'model');
+        $this->user = $this->bodo->Dec($this->session->userdata('id_user'));
+    }
+
+    function AddText($pdf, $text, $x, $y, $a, $f, $t, $s, $r, $g, $b) {
+        $pdf->SetFont($f, $t, $s);
+        $pdf->SetXY($x, $y);
+        $pdf->SetTextColor($r, $g, $b);
+        $pdf->Cell(70, 10, $text, 0, 0, $a);
+    }
+
+    function AddTextTitle($pdf, $text, $x, $y, $a, $f, $t, $s, $r, $g, $b) {
+        $pdf->SetFont($f, $t, $s);
+        $pdf->SetXY($x, $y);
+        $pdf->SetTextColor($r, $g, $b);
+        $pdf->Cell(40, 10, $text, 0, 0, $a);
+    }
+
+    function AddDate($pdf, $text, $x, $y, $a, $f, $t, $s, $r, $g, $b) {
+        $pdf->SetFont($f, $t, $s);
+        $pdf->SetXY($x, $y);
+        $pdf->SetTextColor($r, $g, $b);
+        $pdf->Cell(100, 10, $text, 0, 0, $a);
+    }
+
+    function CreatePage($pdf, $name, $course_name, $date) {
+        $pdf->AddPage('L', PDF_UNIT, ['format' => 'A4', 'Rotate' => -90]);
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetFont('helvetica', 'B', 16);
+
+
+        $pdf->setPrintHeader(false);
+        $pdf->SetFooterMargin(0);
+        $pdf->setPrintFooter(false);
+        $pdf->SetAutoPageBreak(TRUE, 0);
+
+
+        $pdf->SetLeftMargin(0);
+        $pdf->SetTopMargin(0);
+        $pdf->StartTransform();
+        $pdf->Image(FCPATH . 'uploads/imgs/mini-class.png', 0, 0, 0);
+        $pdf->Rotate(360);
+        // $pdf->WriteHTML($html);
+        $pdf->StopTransform();
+        //Add a Name to the certificate
+        $this->AddText($pdf, ucwords($name), 30, 117, 'C', 'Helvetica', 'B', 40, 3, 84, 156);
+        $this->AddTextTitle($pdf, ucwords('Mini Class Sertifikasiku :'), 36, 155, 'C', 'Helvetica', 'B', 18, 3, 84, 156);
+        $this->AddTextTitle($pdf, ucwords($course_name), 44, 165, 'C', 'Helvetica', 'B', 18, 3, 84, 156);
+
+        $this->AddDate($pdf, ucwords($date), 0, 185, 'C', 'Arial', 'B', 14, 255, 255, 255);
+    }
+
+    public function index_get() {
+        $this->response([], REST_Controller::HTTP_OK);
+    }
+
+    public function generate_post() {
+
+        $this->load->library('Pdf');
+        $pdf = new Pdf('L', PDF_UNIT, ['format' => 'A4', 'Rotate' => -90], true, "UTF-8", false);
+
+        $msg = array();
+
+        try {
+            $structure = FCPATH . 'festival/' . date('dmY') . "/";
+            if (!is_dir($structure)) {
+                mkdir($structure, 0777, true);
+            }
+            $res = $this->db->insert('mt_festival', array(
+                'full_name' => $this->input->post('full_name'),
+                'email' => $this->input->post('email'),
+                'course_title' => $this->input->post('course_title'),
+                'certificate_url' => base_url() . 'festival/' . date('dmY') . '/',
+            ));
+            if ($res) {
+                $this->CreatePage($pdf, $this->input->post('full_name'), $this->input->post('course_title'), $this->input->post('tanggal'), '');
+                $pdf->Output($structure . date('dmYHms') . '-festival.pdf', 'F');
+                $msg['status'] = true;
+                $msg['message'] = 'Success';
+            }
+            $this->response($msg, REST_Controller::HTTP_OK);
+        } catch (Exception $e) {
+            $msg['status'] = false;
+            $msg['message'] = 'Fail !';
+            $msg['error'] = $e->getMessage();
+            toJson($msg);
+        }
+    }
+
+}
--- application/modules/Master/views/sponsor/edit.php
+++ application/modules/Master/views/sponsor/edit.php
@@ -3,37 +3,28 @@
         <div class="card-body">
             <form action="<?php echo site_url('Master/Sponsor/Update/'); ?>" method="post" enctype="multipart/form-data">
                 <input type="hidden" name="<?php echo $csrf['name'] ?>" value="<?php echo $csrf['hash'] ?>" />
-                <input type="hidden" name="id" value="<?= $id; ?>" />
+                <input type="hidden" name="id" value="<?php echo $id; ?>" />
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="kategori">Kategori:</label>
                             <select name="kategori" id="kategori" class="form-control">
-                                <option value="1" <?= $data->kategori == 1 ? 'selected' : '' ?>>Sponsor</option>
-                                <option value="2" <?= $data->kategori == 2 ? 'selected' : '' ?>>Media Patner</option>
+                                <option value="1" <?php echo $data->kategori == 1 ? 'selected' : '' ?>>Sponsor</option>
+                                <option value="2" <?php echo $data->kategori == 2 ? 'selected' : '' ?>>Media Patner</option>
                             </select>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="nama">Nama:</label>
-                            <input id="nama" name="nama" type="text" class="form-control" required value="<?= $data->nama; ?>">
+                            <input id="nama" name="nama" type="text" class="form-control" required value="<?php echo $data->nama; ?>">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <div class="image-input image-input-outline" id="kt_image_4" style="background-image:url('<?php echo site_url('assets/images/sponsor/' . $data->path); ?>');">
                                 <div class="image-input-wrapper"></div>
-                                <label class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow" data-action="change" data-toggle="tooltip" title="" data-original-title="Change picture">
-                                    <i class="fa fa-pen icon-sm text-muted"></i>
-                                    <input type="file" name="path" accept=".png, .jpg, .jpeg" value="<?php echo $data->path; ?>" />
-                                    <input type="hidden" name="sponsor_remove" />
-                                    <input type="hidden" name="old_ava" value="<?php echo $data->path; ?>" />
-                                </label>
                                 <span class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow" data-action="cancel" data-toggle="tooltip" title="Cancel picture">
-                                    <i class="fas fa-times icon-xs text-muted"></i>
-                                </span>
-                                <span class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow" data-action="remove" data-toggle="tooltip" title="Remove picture">
                                     <i class="fas fa-times icon-xs text-muted"></i>
                                 </span>
                             </div>
@@ -42,7 +33,7 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="url_website">Url Website:</label>
-                            <input id="url_website" name="url_website" type="text" class="form-control" required value="<?= $data->url_website; ?>">
+                            <input id="url_website" name="url_website" type="text" class="form-control" required value="<?php echo $data->url_website; ?>">
                         </div>
                     </div>
                 </div>
