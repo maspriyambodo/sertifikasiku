eec894dba72d167ebc774e34a3f2ad22d266d168
maspriyambodo <maspriyambodo@gmail.com>
2021-10-31 23:27:12 +0700

    add backup file

---
+++ db_template/bckup/sertif_database.sql
@@ -0,0 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 31, 2021 at 04:24 PM
+-- Server version: 8.0.27
+-- PHP Version: 7.4.25
+
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_sertifikasiku`
+--
+CREATE DATABASE IF NOT EXISTS `db_sertifikasiku` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `db_sertifikasiku`;
---
+++ db_template/bckup/sertif_table_dt_sponsor.sql
@@ -0,0 +1,52 @@
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dt_sponsor`
+--
+
+DROP TABLE IF EXISTS `dt_sponsor`;
+CREATE TABLE `dt_sponsor` (
+  `id` int NOT NULL,
+  `kategori` int DEFAULT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `path` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `url_website` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `stat` int DEFAULT NULL,
+  `syscreateuser` int DEFAULT NULL,
+  `syscreatedate` datetime DEFAULT NULL,
+  `sysupdateuser` int DEFAULT NULL,
+  `sysupdatedate` datetime DEFAULT NULL,
+  `sysdeleteuser` int DEFAULT NULL,
+  `sysdeletedate` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELATIONSHIPS FOR TABLE `dt_sponsor`:
+--
+
+--
+-- Dumping data for table `dt_sponsor`
+--
+
+INSERT INTO `dt_sponsor` (`id`, `kategori`, `nama`, `path`, `url_website`, `stat`, `syscreateuser`, `syscreatedate`, `sysupdateuser`, `sysupdatedate`, `sysdeleteuser`, `sysdeletedate`) VALUES
+(1, 1, 'media partner 1', 'jkashd.png', 'https://facebook.com', 1, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(2, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 1, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(3, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(4, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(5, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(6, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(7, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(8, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(9, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(10, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(11, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(12, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(13, 2, 'media partner 1', 'aosido.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(14, 1, 'media partner 1', 'jkashd.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(15, 1, 'media partner 1', 'jkashd.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(16, 1, 'media partner 1', 'jkashd.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(17, 1, 'media partner 1', 'jkashd.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(18, 1, 'media partner 1', 'jkashd.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(19, 1, 'media partner 1', 'jkashd.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL),
+(20, 1, 'media partner 1', 'jkashd.png', 'https://facebook.com', 0, 1, '2021-10-28 22:05:24', NULL, NULL, NULL, NULL);
