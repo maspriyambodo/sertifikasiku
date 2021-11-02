2b965411d4c6a7c5a1eae300a302c3ce3008dbf8
maspriyambodo <maspriyambodo@gmail.com>
2021-11-02 19:19:17 +0700

    update sponsor add

--- application/modules/Master/views/sponsor/modal_add.php
+++ application/modules/Master/views/sponsor/modal_add.php
@@ -13,8 +13,9 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="kategori">Kategori:</label>
-                                <select name="kategori" id="kategori" class="form-control" required>
+                                <label for="kategori">Category:</label>
+                                <select name="kategori" id="kategori" class="form-control" required="">
+                                    <option value="">select category</option>
                                     <option value="<?php echo Enkrip(1); ?>">Sponsor</option>
                                     <option value="<?php echo Enkrip(2); ?>">Media Patner</option>
                                 </select>
@@ -22,54 +23,37 @@
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="nama">Nama:</label>
-                                <input id="nama" name="nama" type="text" class="form-control" required autocomplete="off"/>
+                                <label for="nama">Name:</label>
+                                <input id="nama" name="nama" type="text" class="form-control" required="" autocomplete="off"/>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="path">Gambar:</label>
-                                <input id="path" name="path" type="file" class="form-control" required>
+                                <label for="path">Logo:</label>
+                                <input id="path" name="path" type="file" class="form-control" required=""/>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="url_website">Url Website:</label>
-                                <input id="url_website" name="url_website" type="text" class="form-control" required autocomplete="off"/>
+                                <input id="url_website" name="url_website" type="text" class="form-control" required="" autocomplete="off"/>
                             </div>
                         </div>
                     </div>
-
-
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-success" onclick="Save()">Save</button>
+                    <button type="submit" class="btn btn-success">Save</button>
                     <button type="button" class="btn btn-default" data-dismiss="modal" onclick="Close_add()">No</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
-
-<script type="text/javascript">
-    $(function () {
-        $("#start_date").datepicker({
-            autoclose: true,
-            todayHighlight: true
-        });
-        $("#end_date").datepicker({
-            autoclose: true,
-            todayHighlight: true
-        });
-    });
-</script>
 <script>
     function Close_add() {
-    }
-
-    function Save() {
-
-        $("#form_add").submit();
-
+        $('select[name="kategori"]').val('');
+        $('input[name="nama"]').val('');
+        $('input[name="path"]').val('');
+        $('input[name="url_website"]').val('');
     }
 </script>
